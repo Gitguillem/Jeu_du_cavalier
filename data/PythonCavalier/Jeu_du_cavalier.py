7be002894cba93446471e56a28637846e922a5ef
AnthonyP2 <100562798+AnthonyP2@users.noreply.github.com>
2022-05-03 10:08:13 +0200

    Add files via upload

--- data/PythonCavalier/Jeu_du_cavalier.py
+++ data/PythonCavalier/Jeu_du_cavalier.py
@@ -6,14 +6,18 @@ from random import *
 from pygame.locals import *
 import pygame
 pygame.init()
+pygame.mixer.init()
 
 # Initialisation des images et sons
 game_win = pygame.image.load("GAME_W.jpg")
 game_over = pygame.image.load("GAME_OVER.jpg")
 bouse = pygame.image.load("bouse.gif")
 cavalier = pygame.image.load("cavalier.gif")
-VicSFX = pygame.mixer.Sound("VictorySFX.wav")
+VicSFX = pygame.mixer.Sound("VicSFX.wav")
 LossSFX = pygame.mixer.Sound("LossSFX.wav")
+backSFX = pygame.mixer.Sound("backgroundSFX.wav")
+pygame.mixer.music.load("backgroundSFX.wav")
+pygame.mixer.music.play(-1)
 
 # Initialisation de la fenetre
 largeur = 800
@@ -30,6 +34,7 @@ cy = 0
 AVA = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
 
 # Initialisation des parametres
+once = 0
 BLCK = 0, 0, 0
 WHITE = 255, 255, 255
 C = BLCK
@@ -66,29 +71,33 @@ GRILLE[gri_pos_y] [gri_pos_x] = 1
 
 #Génération et vérification des positions diponibles:
 def positions_dispo():
-    non_dispos = 0
+    dispos = 8
     for i in range (len(AVA)):
         rx = cx + AVA[i][0]
         ry = cy + AVA[i][1]
+        #print("RX:",rx,"|| RY:",ry)
         try:
-            if GRILLE[ry][rx] == 2:
-                non_dispos += 1
+            if ry<0 or rx<0:
+                dispos = dispos - 1
             elif GRILLE[ry][rx] == 0:
-                non_dispos += 0
+                dispos = dispos
+            elif GRILLE[ry][rx] != 0:
+                dispos = dispos - 1
         except IndexError:
-            non_dispos += 1
+            dispos = dispos - 1
 
-    return non_dispos
+    return dispos
 
 
 def check_pos():
-    non_dispos = positions_dispo()
-    print(non_dispos)
-    if non_dispos != 8:
-        None
-    elif non_dispos == 8:
+    dispos = positions_dispo()
+    print(dispos)
+    if dispos != 0:
+        running = True
+    elif dispos == 0:
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_over, (0, 0))
+        pygame.mixer.music.stop()
         pygame.mixer.Sound.play(LossSFX)
         pygame.mixer.music.stop()
 
@@ -96,6 +105,7 @@ def victoire():
     if compteur >= 64:
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_win, (0, 0))
+        pygame.mixer.music.stop()
         pygame.mixer.Sound.play(VicSFX)
         pygame.mixer.music.stop()
 
@@ -196,7 +206,7 @@ running = True
 
 while running:
     # Limitation du nombre de tours de boucle par seconde
-    clock.tick(10)
+    clock.tick(60)
     # Boucle des evennements
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -210,14 +220,14 @@ while running:
     # Lorsqu'on clique:
 
     if event.type == MOUSEBUTTONDOWN and event.button==1:
-
+        # Vérification de perdu ou gagné
+        victoire()
+        check_pos()
         mouse_pos = pygame.mouse.get_pos()
         m_x = mouse_pos[0] // 100
         m_y = mouse_pos[1] // 100
-
         if GRILLE[m_y][m_x] == 2:
             print("Position indisponible")
-
         elif GRILLE[m_y][m_x] == 0:
             for i in range (len(AVA)):
                 if m_x == cx + AVA[i][0]:
@@ -231,9 +241,7 @@ while running:
                         current_pos = new_pos
                         compteur += 1
 
-    # Vérification de perdu ou gagné
-    victoire()
-    check_pos()
+
 
 
     pygame.display.flip()
