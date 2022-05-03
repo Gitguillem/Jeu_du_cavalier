b14e4481edd577310effba53c219e3dcac294f01
AnthonyP2 <100562798+AnthonyP2@users.noreply.github.com>
2022-05-03 07:06:38 +0200

    Add files via upload

--- data/PythonCavalier/Jeu_du_cavalier.py
+++ data/PythonCavalier/Jeu_du_cavalier.py
@@ -1,26 +1,26 @@
 ﻿#Créé le 10/03/2022 à 12:35 CET
-#Créé par Anthony Presman
+#Créé par Anthony, Íngrid et Thalia
 #Projet: Jeu du cavalier
 
 from random import *
-# Importation de pygame
 from pygame.locals import *
 import pygame
 pygame.init()
+
+# Initialisation des images et sons
+game_win = pygame.image.load("GAME_W.jpg")
+game_over = pygame.image.load("GAME_OVER.jpg")
+bouse = pygame.image.load("bouse.gif")
+cavalier = pygame.image.load("cavalier.gif")
+VicSFX = pygame.mixer.Sound("VictorySFX.wav")
+LossSFX = pygame.mixer.Sound("LossSFX.wav")
 
 # Initialisation de la fenetre
 largeur = 800
 hauteur = 800
 windowSurface = pygame.display.set_mode((largeur, hauteur), 0,32)
 pygame.display.set_caption("Jeu du cavalier | Créé par Anthony, Íngrid, Thalia")
-game_win = pygame.image.load("GAME_W.jpg")
-game_over = pygame.image.load("GAME_OVER.jpg")
-bouse = pygame.image.load("bouse.gif")
-cavalier = pygame.image.load("cavalier.gif")
 pygame.display.set_icon(cavalier)
-bouse_rect = bouse.get_rect()
-compteur = 1
-
 
 #Variable de la position courrante du cavalier
 cx = 0
@@ -33,10 +33,9 @@ AVA = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
 BLCK = 0, 0, 0
 WHITE = 255, 255, 255
 C = BLCK
+compteur = 1
 
-
-
-
+# Génération de la position initiale
 def start_pos ():
     x = randint(0,7) * 100 + 18
     y = randint(0,7) * 100 + 17
@@ -46,8 +45,7 @@ current_pos = (start)
 (cx, cy) = (start)
 current_pos = (cx, cy)
 
-
-
+# Définition de la grille de jeu
 GRILLE = [
 [0,0,0,0,0,0,0,0],
 [0,0,0,0,0,0,0,0],
@@ -59,14 +57,14 @@ GRILLE = [
 [0,0,0,0,0,0,0,0]
            ]
 
-
+# Position initiale dans la grille
 gri_pos_x = round(cx / 100)
 gri_pos_y = round(cy / 100)
 cx = cx // 100
 cy = cy // 100
 GRILLE[gri_pos_y] [gri_pos_x] = 1
 
-#Vérification des positions diponibles:
+#Génération et vérification des positions diponibles:
 def positions_dispo():
     non_dispos = 0
     for i in range (len(AVA)):
@@ -85,14 +83,21 @@ def positions_dispo():
 
 def check_pos():
     non_dispos = positions_dispo()
+    print(non_dispos)
     if non_dispos != 8:
         None
     elif non_dispos == 8:
+        pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_over, (0, 0))
+        pygame.mixer.Sound.play(LossSFX)
+        pygame.mixer.music.stop()
 
 def victoire():
     if compteur >= 64:
+        pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_win, (0, 0))
+        pygame.mixer.Sound.play(VicSFX)
+        pygame.mixer.music.stop()
 
 
 pygame.display.flip()
@@ -206,14 +211,13 @@ while running:
 
     if event.type == MOUSEBUTTONDOWN and event.button==1:
 
-        cavalier = cavalier
         mouse_pos = pygame.mouse.get_pos()
         m_x = mouse_pos[0] // 100
         m_y = mouse_pos[1] // 100
-        cords = (m_x,m_y)
-        old_cords = (cx,cy)
+
         if GRILLE[m_y][m_x] == 2:
             print("Position indisponible")
+
         elif GRILLE[m_y][m_x] == 0:
             for i in range (len(AVA)):
                 if m_x == cx + AVA[i][0]:
@@ -230,6 +234,7 @@ while running:
     # Vérification de perdu ou gagné
     victoire()
     check_pos()
+
 
     pygame.display.flip()
 
