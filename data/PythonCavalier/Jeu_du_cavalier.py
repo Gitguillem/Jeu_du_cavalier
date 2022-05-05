07f7bef056cca39988d9d4e17f5c09a2cb6f392d
AnthonyP2 <100562798+AnthonyP2@users.noreply.github.com>
2022-05-05 12:52:14 +0200

    Add files via upload

--- data/PythonCavalier/Jeu_du_cavalier.py
+++ data/PythonCavalier/Jeu_du_cavalier.py
@@ -2,11 +2,21 @@
 #Créé par Anthony, Íngrid et Thalia
 #Projet: Jeu du cavalier
 
+#Importation des librairies et initialisation des paramètres
 from random import *
 from pygame.locals import *
+import wave
 import pygame
 pygame.init()
 pygame.mixer.init()
+
+#Son de fond
+file_path = 'backgroundSFX.wav'
+file_wav = wave.open(file_path)
+frequency = file_wav.getframerate()
+pygame.mixer.init(frequency=frequency)
+pygame.mixer.music.load(file_path)
+pygame.mixer.music.play(-1)
 
 # Initialisation des images et sons
 game_win = pygame.image.load("GAME_W.jpg")
@@ -15,9 +25,7 @@ bouse = pygame.image.load("bouse.gif")
 cavalier = pygame.image.load("cavalier.gif")
 VicSFX = pygame.mixer.Sound("VicSFX.wav")
 LossSFX = pygame.mixer.Sound("LossSFX.wav")
-backSFX = pygame.mixer.Sound("backgroundSFX.wav")
-pygame.mixer.music.load("backgroundSFX.wav")
-pygame.mixer.music.play(-1)
+
 
 # Initialisation de la fenetre
 largeur = 800
@@ -32,6 +40,9 @@ cy = 0
 
 #Tableau des mouvements du cavalier
 AVA = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
+
+#Tableau des positions usées
+used_pos = []
 
 # Initialisation des parametres
 once = 0
@@ -67,6 +78,7 @@ gri_pos_x = round(cx / 100)
 gri_pos_y = round(cy / 100)
 cx = cx // 100
 cy = cy // 100
+old_cords = (cx * 100, cy*100)
 GRILLE[gri_pos_y] [gri_pos_x] = 1
 
 #Génération et vérification des positions diponibles:
@@ -88,21 +100,23 @@ def positions_dispo():
 
     return dispos
 
-
 def check_pos():
     dispos = positions_dispo()
     print(dispos)
     if dispos != 0:
         running = True
     elif dispos == 0:
+        running = False
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_over, (0, 0))
         pygame.mixer.music.stop()
         pygame.mixer.Sound.play(LossSFX)
         pygame.mixer.music.stop()
 
+#Vérification de victoire
 def victoire():
     if compteur >= 64:
+        running = False
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_win, (0, 0))
         pygame.mixer.music.stop()
@@ -111,92 +125,6 @@ def victoire():
 
 
 pygame.display.flip()
-
-
-    # Elements a tracer
-        #Affichage des carrés blancs:
-pygame.draw.rect(windowSurface,WHITE, (700, 000, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (700, 600, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (700, 400, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (700, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (600, 700, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (600, 500, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (600, 300, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (600, 100, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (500, 000, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (500, 600, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (500, 400, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (500, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (400, 700, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (400, 500, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (400, 300, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (400, 100, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (300, 000, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (300, 600, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (300, 400, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (300, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (200, 700, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (200, 500, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (200, 300, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (200, 100, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (100, 000, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (100, 600, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (100, 400, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (100, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,WHITE, (000, 700, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (000, 500, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (000, 300, 100, 100),)
-pygame.draw.rect(windowSurface,WHITE, (000, 100, 100, 100),)
-
-#Tracer les carrés noirs
-pygame.draw.rect(windowSurface,C, (600, 00, 100, 100),)
-pygame.draw.rect(windowSurface,C, (600, 600, 100, 100),)
-pygame.draw.rect(windowSurface,C, (600, 400, 100, 100),)
-pygame.draw.rect(windowSurface,C, (600, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (500, 100, 100, 100),)
-pygame.draw.rect(windowSurface,C, (500, 700, 100, 100),)
-pygame.draw.rect(windowSurface,C, (500, 500, 100, 100),)
-pygame.draw.rect(windowSurface,C, (500, 300, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (700, 100, 100, 100),)
-pygame.draw.rect(windowSurface,C, (700, 700, 100, 100),)
-pygame.draw.rect(windowSurface,C, (700, 500, 100, 100),)
-pygame.draw.rect(windowSurface,C, (700, 300, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (400, 00, 100, 100),)
-pygame.draw.rect(windowSurface,C, (400, 600, 100, 100),)
-pygame.draw.rect(windowSurface,C, (400, 400, 100, 100),)
-pygame.draw.rect(windowSurface,C, (400, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (300, 100, 100, 100),)
-pygame.draw.rect(windowSurface,C, (300, 700, 100, 100),)
-pygame.draw.rect(windowSurface,C, (300, 500, 100, 100),)
-pygame.draw.rect(windowSurface,C, (300, 300, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (200, 00, 100, 100),)
-pygame.draw.rect(windowSurface,C, (200, 600, 100, 100),)
-pygame.draw.rect(windowSurface,C, (200, 400, 100, 100),)
-pygame.draw.rect(windowSurface,C, (200, 200, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (100, 100, 100, 100),)
-pygame.draw.rect(windowSurface,C, (100, 700, 100, 100),)
-pygame.draw.rect(windowSurface,C, (100, 500, 100, 100),)
-pygame.draw.rect(windowSurface,C, (100, 300, 100, 100),)
-
-pygame.draw.rect(windowSurface,C, (000, 00, 100, 100),)
-pygame.draw.rect(windowSurface,C, (000, 600, 100, 100),)
-pygame.draw.rect(windowSurface,C, (000, 400, 100, 100),)
-pygame.draw.rect(windowSurface,C, (000, 200, 100, 100),)
-
-
 
 
 # Boucle de jeu
@@ -211,38 +139,119 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # AFFICHAGE DU CADRILLAGE
+        # Affichage des carrés blancs:
+    pygame.draw.rect(windowSurface,WHITE, (700, 000, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (700, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (700, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (700, 200, 100, 100),)
 
+    pygame.draw.rect(windowSurface,WHITE, (600, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (600, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (600, 300, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (600, 100, 100, 100),)
 
+    pygame.draw.rect(windowSurface,WHITE, (500, 000, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (500, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (500, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (500, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,WHITE, (400, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (400, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (400, 300, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (400, 100, 100, 100),)
+
+    pygame.draw.rect(windowSurface,WHITE, (300, 000, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (300, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (300, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (300, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,WHITE, (200, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (200, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (200, 300, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (200, 100, 100, 100),)
+
+    pygame.draw.rect(windowSurface,WHITE, (100, 000, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (100, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (100, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (100, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,WHITE, (000, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (000, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (000, 300, 100, 100),)
+    pygame.draw.rect(windowSurface,WHITE, (000, 100, 100, 100),)
+
+        # Tracer les carrés noirs
+    pygame.draw.rect(windowSurface,C, (600, 00, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (600, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (600, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (600, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (500, 100, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (500, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (500, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (500, 300, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (700, 100, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (700, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (700, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (700, 300, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (400, 00, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (400, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (400, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (400, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (300, 100, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (300, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (300, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (300, 300, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (200, 00, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (200, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (200, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (200, 200, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (100, 100, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (100, 700, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (100, 500, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (100, 300, 100, 100),)
+
+    pygame.draw.rect(windowSurface,C, (000, 00, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (000, 600, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (000, 400, 100, 100),)
+    pygame.draw.rect(windowSurface,C, (000, 200, 100, 100),)
 
     # DESSINAGE DU CAVALIER
     windowSurface.blit(cavalier, current_pos)
 
-    # Lorsqu'on clique:
+    # Dessinage de la bouse
+    for i in range (len(used_pos)):
+        windowSurface.blit(bouse, (used_pos[i][0] + 16, used_pos[i][1] + 34))
 
+    # Lorsqu'on clique:
     if event.type == MOUSEBUTTONDOWN and event.button==1:
         # Vérification de perdu ou gagné
         victoire()
         check_pos()
         mouse_pos = pygame.mouse.get_pos()
+        old_cords=(cx * 100, cy*100)
         m_x = mouse_pos[0] // 100
         m_y = mouse_pos[1] // 100
         if GRILLE[m_y][m_x] == 2:
-            print("Position indisponible")
+            None
         elif GRILLE[m_y][m_x] == 0:
             for i in range (len(AVA)):
                 if m_x == cx + AVA[i][0]:
                     if m_y == cy + AVA[i][1]:
                         GRILLE[cy][cx] = 2
-                        windowSurface.blit(bouse, (cx*100 + 16, cy*100 + 34))
+                        used_pos.append(old_cords)
                         cx = m_x
                         cy = m_y
                         GRILLE[cy][cx] = 1
                         new_pos = (cx * 100 + 18, cy * 100 + 17)
                         current_pos = new_pos
                         compteur += 1
-
-
-
 
     pygame.display.flip()
 
