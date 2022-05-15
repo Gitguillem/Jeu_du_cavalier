af41adf7db9c2fab9e217a0d48a7565e6f2ae0b4
AnthonyP2 <100562798+AnthonyP2@users.noreply.github.com>
2022-05-15 22:54:11 +0200

    Add files via upload

--- data/PythonCavalier/Jeu_du_cavalier.py
+++ data/PythonCavalier/Jeu_du_cavalier.py
@@ -25,7 +25,7 @@ bouse = pygame.image.load("bouse.gif")
 cavalier = pygame.image.load("cavalier.gif")
 VicSFX = pygame.mixer.Sound("VicSFX.wav")
 LossSFX = pygame.mixer.Sound("LossSFX.wav")
-LossSFX.set_volume(0.5)
+LossSFX.set_volume(0.2)
 
 # Initialisation de la fenetre
 largeur = 800
@@ -34,7 +34,7 @@ windowSurface = pygame.display.set_mode((largeur, hauteur), 0,32)
 pygame.display.set_caption("Jeu du cavalier | Créé par Anthony, Íngrid, Thalia")
 pygame.display.set_icon(cavalier)
 
-#Variables de perte et victoire
+#Variable de la victoire
 gagne = 0
 
 #Variable de la position courrante du cavalier
@@ -44,11 +44,10 @@ cy = 0
 #Tableau des mouvements du cavalier
 AVA = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
 
-#Tableau des positions usées
+#Tableau des positions où le cavalier a déjà été
 used_pos = []
 
 # Initialisation des parametres
-once = 0
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 compteur = 1
@@ -171,7 +170,6 @@ def positions_dispo():
     for i in range (len(AVA)):
         rx = cx + AVA[i][0]
         ry = cy + AVA[i][1]
-        #print("RX:",rx,"|| RY:",ry)
         try:
             if ry<0 or rx<0:
                 dispos = dispos - 1
@@ -181,12 +179,10 @@ def positions_dispo():
                 dispos = dispos - 1
         except IndexError:
             dispos = dispos - 1
-
     return dispos
 
 def perte():
     dispos = positions_dispo()
-    print(dispos)
     if dispos != 0:
         running = True
     elif dispos == 0:
@@ -213,10 +209,9 @@ while running:
     # Limitation du nombre de tours de boucle par seconde
     clock.tick(60)
     # Boucle des evennements
-    for event in pygame.event.get():
+    for event in pygame.event.get():                    # Si on clique sur la croix pour quitter l'application
         if event.type == pygame.QUIT:
             running = False
-
 
     # Dessinage du cavalier
     windowSurface.blit(cavalier, current_pos)
@@ -225,20 +220,17 @@ while running:
     for i in range (len(used_pos)):
         windowSurface.blit(bouse, (used_pos[i][0] + 16, used_pos[i][1] + 34))
 
-
     # Vérification de perdu ou gagné
     v = victoire()
     p = perte()
-    if v == 1:
-        clock.tick(0)
-        pygame.event.set_blocked(pygame.MOUSEMOTION)
-        windowSurface.blit(game_win, (0, 0))
-        pygame.mixer.music.stop()
-        pygame.mixer.Sound.play(VicSFX)
+    if v == 1:                                          # Si on gagne
+        pygame.event.set_blocked(pygame.MOUSEMOTION)    # Arrête l'intérprétation des clicks
+        windowSurface.blit(game_win, (0, 0))            # Montre l'écran de victoire
+        pygame.mixer.music.stop()                       # Arrête la musique de fond
+        pygame.mixer.Sound.play(VicSFX)                 # Joue le son de victoire
         pygame.mixer.music.stop()
 
-    elif p == 0:
-        clock.tick(0)
+    elif p == 0:                                        # Si on perd
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_over, (0, 0))
         pygame.mixer.music.stop()
@@ -248,15 +240,15 @@ while running:
 
     # Lorsqu'on clique:
     if event.type == MOUSEBUTTONDOWN and event.button==1:
-        mouse_pos = pygame.mouse.get_pos()
-        old_cords=(cx * 100, cy*100)
-        m_x = mouse_pos[0] // 100
+        mouse_pos = pygame.mouse.get_pos()              # Obtention des coordonnées du click
+        old_cords=(cx * 100, cy*100)                    # Transformation des vieilles coordonnées (de la grille) en coordonnées de l'écran
+        m_x = mouse_pos[0] // 100                       # Transformation des coordonnées de la souris en coordonnées de la grille
         m_y = mouse_pos[1] // 100
-        if GRILLE[m_y][m_x] == 2: # Vérification que la grille cliquée ne soit pas déjà occupée
+        if GRILLE[m_y][m_x] == 2:                       # Vérification que le carré cliqué ne soit pas déjà occupé
             None
-        elif GRILLE[m_y][m_x] == 0: # Si le carré cliquée n'est pas déjà occupée
+        elif GRILLE[m_y][m_x] == 0:                     # Si le carré cliqué n'est pas déjà occupé
             for i in range (len(AVA)):
-                if m_x == cx + AVA[i][0]: # Si les coordonnées cliquées correspondent à un des mouvements du cavalier
+                if m_x == cx + AVA[i][0]:               # Si les coordonnées cliquées correspondent à un des mouvements du cavalier
                     if m_y == cy + AVA[i][1]:
                         GRILLE[cy][cx] = 2              # Ancien carreau devient occupé
                         used_pos.append(old_cords)      # On ajoute les coordonnées de l'ancien carré à une liste (pour dessiner les bouses)
@@ -267,7 +259,7 @@ while running:
                         current_pos = new_pos
                         compteur += 1                   # On marque le mouvement en augmentant le compteur de 1
 
-                        # Mise à jour de l'écran
+                        # Mise à jour des carreaux sur l'écran (élimination des images précédentes)
                         pygame.draw.rect(windowSurface,WHITE, (700, 000, 100, 100),)
                         pygame.draw.rect(windowSurface,WHITE, (700, 600, 100, 100),)
                         pygame.draw.rect(windowSurface,WHITE, (700, 400, 100, 100),)
