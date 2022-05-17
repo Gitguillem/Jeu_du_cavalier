fd97b996cb219db0f4f64e90700bd567a7fc2808
AnthonyP2 <100562798+AnthonyP2@users.noreply.github.com>
2022-05-17 09:57:05 +0200

    Ajouté des commentaires

--- data/PythonCavalier/Jeu_du_cavalier.py
+++ data/PythonCavalier/Jeu_du_cavalier.py
@@ -7,6 +7,7 @@ from random import *
 from pygame.locals import *
 import wave
 import pygame
+import time
 pygame.init()
 pygame.mixer.init()
 
@@ -26,6 +27,7 @@ cavalier = pygame.image.load("cavalier.gif")
 VicSFX = pygame.mixer.Sound("VicSFX.wav")
 LossSFX = pygame.mixer.Sound("LossSFX.wav")
 LossSFX.set_volume(0.2)
+VicSFX.set_volume(0.7)
 
 # Initialisation de la fenetre
 largeur = 800
@@ -124,7 +126,7 @@ pygame.draw.rect(windowSurface,WHITE, (000, 300, 100, 100),)
 pygame.draw.rect(windowSurface,WHITE, (000, 100, 100, 100),)
 
     # Tracer les carrés noirs
-pygame.draw.rect(windowSurface,BLACK, (600, 00, 100, 100),)
+pygame.draw.rect(windowSurface,BLACK, (600, 000, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (600, 600, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (600, 400, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (600, 200, 100, 100),)
@@ -139,7 +141,7 @@ pygame.draw.rect(windowSurface,BLACK, (700, 700, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (700, 500, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (700, 300, 100, 100),)
 
-pygame.draw.rect(windowSurface,BLACK, (400, 00, 100, 100),)
+pygame.draw.rect(windowSurface,BLACK, (400, 000, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (400, 600, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (400, 400, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (400, 200, 100, 100),)
@@ -159,57 +161,56 @@ pygame.draw.rect(windowSurface,BLACK, (100, 700, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (100, 500, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (100, 300, 100, 100),)
 
-pygame.draw.rect(windowSurface,BLACK, (000, 00, 100, 100),)
+pygame.draw.rect(windowSurface,BLACK, (000, 000, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (000, 600, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (000, 400, 100, 100),)
 pygame.draw.rect(windowSurface,BLACK, (000, 200, 100, 100),)
 
 #Génération et vérification des positions diponibles:
 def positions_dispo():
-    dispos = 8
-    for i in range (len(AVA)):
+    dispos = 8                                              # Numéro max de positions disponibles pour le cavalier à un moment
+    for i in range (len(AVA)):                              # Création des positions théoriquement disponibles
         rx = cx + AVA[i][0]
         ry = cy + AVA[i][1]
-        try:
-            if ry<0 or rx<0:
+        try:                                                # On essaye de vérifier les positions
+            if ry<0 or rx<0:                                # Comme GRILLE est un tableau, il va intérpreter un index de -1 comme la dérnière valeur du tableau, on va donc l'ignorer
                 dispos = dispos - 1
-            elif GRILLE[ry][rx] == 0:
+            elif GRILLE[ry][rx] == 0:                       # Si la case est disponible, on ne change rien
                 dispos = dispos
-            elif GRILLE[ry][rx] != 0:
+            elif GRILLE[ry][rx] != 0:                       # Si la case est occupée, on diminue la qté de positions disponibles de 1
                 dispos = dispos - 1
-        except IndexError:
+        except IndexError:                                  # Si le numéro à un index en dehors des valeurs de la liste (comme -2), on diminue les positions disponibles de 1
             dispos = dispos - 1
     return dispos
 
 def perte():
     dispos = positions_dispo()
-    if dispos != 0:
+    if dispos != 0:                                         # Si il y a plus de 0 positions disponibles, on continue le jeu
         running = True
-    elif dispos == 0:
-        pygame.event.set_blocked(pygame.MOUSEMOTION)
+    elif dispos == 0:                                       # Sinon, on arrête le jeu
+        pygame.event.set_blocked(pygame.MOUSEMOTION)        # On arrête l'intérprétation des clicks de la souris
         return dispos
 
 #Vérification de victoire
 def victoire():
-    if compteur >= 64:
-        pygame.event.set_blocked(pygame.MOUSEMOTION)
+    if compteur >= 64:                                      # Si le compteur arrive à 64 (seule option pour gagner)
+        pygame.event.set_blocked(pygame.MOUSEMOTION)        # On arrête l'intérprétation des clicks de la souris
         gagne = 1
         return gagne
 
 
-pygame.display.flip()
+pygame.display.flip()                                       # Mise à jour de l'écran
 
 
-# Boucle de jeu
 clock = pygame.time.Clock()
 running = True
 
-
+# Boucle de jeu
 while running:
     # Limitation du nombre de tours de boucle par seconde
     clock.tick(60)
     # Boucle des evennements
-    for event in pygame.event.get():                    # Si on clique sur la croix pour quitter l'application
+    for event in pygame.event.get():                        # Si on clique sur la croix pour quitter l'application
         if event.type == pygame.QUIT:
             running = False
 
@@ -221,43 +222,44 @@ while running:
         windowSurface.blit(bouse, (used_pos[i][0] + 16, used_pos[i][1] + 34))
 
     # Vérification de perdu ou gagné
-    v = victoire()
+    v = victoire()                                          # On obtient les valeurs des variables de perte et victoire
     p = perte()
-    if v == 1:                                          # Si on gagne
-        pygame.event.set_blocked(pygame.MOUSEMOTION)    # Arrête l'intérprétation des clicks
-        windowSurface.blit(game_win, (0, 0))            # Montre l'écran de victoire
-        pygame.mixer.music.stop()                       # Arrête la musique de fond
-        pygame.mixer.Sound.play(VicSFX)                 # Joue le son de victoire
+    if v == 1:                                              # Si on gagne
+        pygame.event.set_blocked(pygame.MOUSEMOTION)        # Arrête l'intérprétation des clicks
+        windowSurface.blit(game_win, (0, 0))                # Montre l'écran de victoire
+        pygame.mixer.music.stop()                           # Arrête la musique de fond
+        pygame.mixer.Sound.play(VicSFX)                     # Joue le son de victoire
         pygame.mixer.music.stop()
+        pygame.mixer.stop()
 
-    elif p == 0:                                        # Si on perd
+    elif p == 0:                                            # Si on perd
         pygame.event.set_blocked(pygame.MOUSEMOTION)
         windowSurface.blit(game_over, (0, 0))
         pygame.mixer.music.stop()
-        LossSFX.play(1)
+        LossSFX.play(0,3000)
         pygame.mixer.music.stop()
 
 
     # Lorsqu'on clique:
     if event.type == MOUSEBUTTONDOWN and event.button==1:
-        mouse_pos = pygame.mouse.get_pos()              # Obtention des coordonnées du click
-        old_cords=(cx * 100, cy*100)                    # Transformation des vieilles coordonnées (de la grille) en coordonnées de l'écran
-        m_x = mouse_pos[0] // 100                       # Transformation des coordonnées de la souris en coordonnées de la grille
+        mouse_pos = pygame.mouse.get_pos()                  # Obtention des coordonnées du click
+        old_cords=(cx * 100, cy*100)                        # Transformation des vieilles coordonnées (de la grille) en coordonnées de l'écran
+        m_x = mouse_pos[0] // 100                           # Transformation des coordonnées de la souris en coordonnées de la grille
         m_y = mouse_pos[1] // 100
-        if GRILLE[m_y][m_x] == 2:                       # Vérification que le carré cliqué ne soit pas déjà occupé
+        if GRILLE[m_y][m_x] == 2:                           # Vérification que le carré cliqué ne soit pas déjà occupé
             None
-        elif GRILLE[m_y][m_x] == 0:                     # Si le carré cliqué n'est pas déjà occupé
+        elif GRILLE[m_y][m_x] == 0:                         # Si le carré cliqué n'est pas déjà occupé
             for i in range (len(AVA)):
-                if m_x == cx + AVA[i][0]:               # Si les coordonnées cliquées correspondent à un des mouvements du cavalier
+                if m_x == cx + AVA[i][0]:                   # Si les coordonnées cliquées correspondent à un des mouvements du cavalier
                     if m_y == cy + AVA[i][1]:
-                        GRILLE[cy][cx] = 2              # Ancien carreau devient occupé
-                        used_pos.append(old_cords)      # On ajoute les coordonnées de l'ancien carré à une liste (pour dessiner les bouses)
-                        cx = m_x                        # Les coordonnées du cavalier sont changées pour celles où on a cliqué
+                        GRILLE[cy][cx] = 2                  # Ancien carreau devient occupé
+                        used_pos.append(old_cords)          # On ajoute les coordonnées de l'ancien carré à une liste (pour dessiner les bouses)
+                        cx = m_x                            # Les coordonnées du cavalier sont changées pour celles où on a cliqué
                         cy = m_y
-                        GRILLE[cy][cx] = 1              # On définit le nouveau carré comme celui utilisé
+                        GRILLE[cy][cx] = 1                  # On définit le nouveau carré comme celui utilisé
                         new_pos = (cx * 100 + 18, cy * 100 + 17)    # Positions pour l'affichage du cavalier (avec centration de l'image)
                         current_pos = new_pos
-                        compteur += 1                   # On marque le mouvement en augmentant le compteur de 1
+                        compteur += 1                       # On marque le mouvement en augmentant le compteur de 1
 
                         # Mise à jour des carreaux sur l'écran (élimination des images précédentes)
                         pygame.draw.rect(windowSurface,WHITE, (700, 000, 100, 100),)
@@ -301,7 +303,7 @@ while running:
                         pygame.draw.rect(windowSurface,WHITE, (000, 100, 100, 100),)
 
                         # Tracer les carrés noirs
-                        pygame.draw.rect(windowSurface,BLACK, (600, 00, 100, 100),)
+                        pygame.draw.rect(windowSurface,BLACK, (600, 000, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (600, 600, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (600, 400, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (600, 200, 100, 100),)
@@ -316,7 +318,7 @@ while running:
                         pygame.draw.rect(windowSurface,BLACK, (700, 500, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (700, 300, 100, 100),)
 
-                        pygame.draw.rect(windowSurface,BLACK, (400, 00, 100, 100),)
+                        pygame.draw.rect(windowSurface,BLACK, (400, 000, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (400, 600, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (400, 400, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (400, 200, 100, 100),)
@@ -326,7 +328,7 @@ while running:
                         pygame.draw.rect(windowSurface,BLACK, (300, 500, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (300, 300, 100, 100),)
 
-                        pygame.draw.rect(windowSurface,BLACK, (200, 00, 100, 100),)
+                        pygame.draw.rect(windowSurface,BLACK, (200, 000, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (200, 600, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (200, 400, 100, 100),)
                         pygame.draw.rect(windowSurface,BLACK, (200, 200, 100, 100),)
@@ -342,6 +344,6 @@ while running:
                         pygame.draw.rect(windowSurface,BLACK, (000, 200, 100, 100),)
 
 
-    pygame.display.flip()
+    pygame.display.flip()                                   # Mise à jour de l'écran
 
 pygame.quit()
